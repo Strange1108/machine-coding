790e7e065a981291a50d1c19806c3b115f89b5dc
Mahendra Jadav <jadavmahendra972@gmail.com>
2025-03-06 17:50:19 +0530

    Contact Form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const App = () => {
+const ContactForm = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
@@ -73,4 +73,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default ContactForm;

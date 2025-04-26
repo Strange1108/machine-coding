1b5321f5b99a5f1f5743ecf10378f7558267c270
Mahendra Jadav <jadavmahendra972@gmail.com>
2025-04-26 23:11:30 +0530

    Stepper Component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,119 @@
-import { useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
-function Cell({ filled, onClick, isDisabled, label }) {
-  return (
-    <button
-      type="button"
-      aria-label={label}
-      disabled={isDisabled}
-      onClick={onClick}
-      className={`border border-black h-0 pb-[100%] ${
-        filled ? "bg-green-500" : "bg-transparent"
-      }`}
-    />
-  );
-}
+const CheckoutStepper = ({stepsConfig = []}) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+  const [margins, setMargins] = useState({
+    marginLeft: 0,
+    marginRight: 0,
+  });
+  const stepRef = useRef([]);
 
-export default function App() {
-  const [order, setOrder] = useState([]);
-  const [isDeactivating, setIsDeactivating] = useState(false);
+  useEffect(() => {
+    setMargins({
+      marginLeft: stepRef.current[0].offsetWidth / 2,
+      marginRight: stepRef.current[stepsConfig.length - 1].offsetWidth / 2,
+    });
+  }, [stepRef, stepsConfig.length]);
 
-  const config = [
-    [1, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1],
-  ];
+  if (!stepsConfig.length) {
+    return <></>;
+  }
 
-  const deactivateCells = () => {
-    setIsDeactivating(true);
-    const timer = setInterval(() => {
-      setOrder((origOrder) => {
-        const newOrder = origOrder.slice();
-        newOrder.pop();
-
-        if (newOrder.length === 0) {
-          clearInterval(timer);
-          setIsDeactivating(false);
-        }
-
-        return newOrder;
-      });
-    }, 300);
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === stepsConfig.length) {
+        setIsComplete(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
   };
 
-  const activateCells = (index) => {
-    const newOrder = [...order, index];
-    setOrder(newOrder);
-
-    if (newOrder.length === config.flat().filter(Boolean).length) {
-      deactivateCells();
-    }
+  const calculateProgressBarWidth = () => {
+    return ((currentStep - 1) / (stepsConfig.length - 1)) * 100;
   };
 
+  const ActiveComponent = stepsConfig[currentStep - 1]?.Component;
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-gray-100">
-      <div
-        className={`grid max-w-[300px] w-full p-5 gap-5 border border-black grid-cols-${config[0].length}`}
-      >
-        {config.flat().map((value, index) =>
-          value ? (
-            <Cell
-              key={index}
-              label={`Cell ${index}`}
-              filled={order.includes(index)}
-              onClick={() => activateCells(index)}
-              isDisabled={order.includes(index) || isDeactivating}
-            />
-          ) : (
-            <div key={index} className="invisible" />
-          )
-        )}
+    <>
+      <div className="stepper">
+        {stepsConfig.map((step, index) => {
+          return (
+            <div
+              key={step.name}
+              ref={(el) => (stepRef.current[index] = el)}
+              className={`step ${
+                currentStep > index + 1 || isComplete ? "complete" : ""
+              } ${currentStep === index + 1 ? "active" : ""} `}
+            >
+              <div className="step-number">
+                {currentStep > index + 1 || isComplete ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
+              </div>
+              <div className="step-name">{step.name}</div>
+            </div>
+          );
+        })}
+
+        <div
+          className="progress-bar"
+          style={{
+            width: `calc(100%-${margins.marginLeft + margins.marginRight}px)`,
+            marginLeft: margins.marginLeft,
+            marginRight: margins.marginRight,
+          }}
+        >
+          <div
+            className="progress"
+            style={{width: `${calculateProgressBarWidth()}%`}}
+          ></div>
+        </div>
       </div>
+
+      <ActiveComponent />
+
+      {!isComplete && (
+        <button className="btn" onClick={handleNext}>
+          {currentStep === stepsConfig.length ? "Finish" : "Next"}
+        </button>
+      )}
+    </>
+  );
+};
+
+
+const CHECKOUT_STEPS = [
+  {
+    name: "Customer Info",
+    Component: () => <div>Provide your contact details.</div>,
+  },
+  {
+    name: "Shipping Info",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Payment",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+  {
+    name: "Delivered",
+    Component: () => <div> Your order has been delivered.</div>,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <h2>Checkout</h2>
+      <CheckoutStepper stepsConfig={CHECKOUT_STEPS} />
     </div>
   );
 }
+
+export default App;
